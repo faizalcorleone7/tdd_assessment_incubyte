f6f419e78c805fca932a99f874d3238c566b8a95
Roshan Basu <roshanbasu7@gmail.com>
2024-12-28 18:49:36 +0530

    Parsing logic for multiple delimiters

--- lib/calculator.rb
+++ lib/calculator.rb
@@ -1,6 +1,6 @@
 class Calculator
 
-  attr_reader :delimiter, :numbers, :negative_numbers
+  attr_reader :delimiter, :numbers, :negative_numbers, :numbers_string
 
   def initialize
     @numbers = []
@@ -8,22 +8,23 @@ class Calculator
   end
 
   def add(input_string)
-    validate_input_characters(input_string)
-    parse(input_string)
+    @numbers_string = input_string
+    validate_input_characters
+    parse
     validate_derived_numbers
     calculate_sum
   end
 
   private
 
-  def parse(numbers_string)
-    derive_delimiter(numbers_string)
-    derive_numbers(numbers_string)
+  def parse
+    derive_delimiter
+    derive_numbers
   end
 
-  def validate_post_delimiter_derivation(numbers_string)
-    return unless numbers_string.include?(delimiter)
-    last_segment = numbers_string.split(delimiter).reverse.first
+  def validate_post_delimiter_derivation(validation_string)
+    return unless validation_string.include?(delimiter)
+    last_segment = validation_string.split(delimiter).reverse.first
     raise ArgumentError if all_whitespace?(last_segment)
   end
 
@@ -31,27 +32,38 @@ class Calculator
     string.each_char.all? { |char| char.match?(/\s/) }
   end
 
-  def derive_delimiter(numbers_string)
-    parsed_delimiter = nil
+  def derive_delimiter
+    parsed_delimiters = []
     if numbers_string.include?("//")
       segments = numbers_string.split("//")
       lines = segments[1].split("\n")
-      parsed_delimiter = lines.first
+      parsed_delimiters = parse_delimiters(lines.first)
       remaining_lines = lines[1..-1].join("\n")
+      @number_string = remaining_lines
     end
-    @delimiter = parsed_delimiter || ","
-    if parsed_delimiter.nil?
+
+    parsed_delimiters.each { |delimiter|
+      numbers_string.gsub!(delimiter, ",")
+    }
+
+    @delimiter = ","
+    if parsed_delimiters.length.zero?
       validate_post_delimiter_derivation(numbers_string)
     else
       validate_post_delimiter_derivation(remaining_lines) if !remaining_lines.nil? && remaining_lines.length > 0
     end
   end
 
-  def derive_numbers(numbers_string)
+  def parse_delimiters(delimiter_line)
+    return [delimiter_line] unless delimiter_line.include?("][")
+    delimiter_line[1..-2].split("][") rescue []
+  end
+
+  def derive_numbers
     @numbers = numbers_string.split(delimiter).map(&:to_i)
   end
 
-  def validate_input_characters(numbers_string)
+  def validate_input_characters
     alphabetic_pattern = /[a-zA-Z]/
     numbers_string.each_char do |number|
       raise ArgumentError if number =~ alphabetic_pattern
