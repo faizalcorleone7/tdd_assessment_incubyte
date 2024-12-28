0942b8ae11a2b51daa1d1210a9e7e93f24d87c92
Roshan Basu <roshanbasu7@gmail.com>
2024-12-28 14:32:15 +0530

    Test for single number in input - 2

--- spec/calculator_spec.rb
+++ spec/calculator_spec.rb
@@ -42,6 +42,12 @@ RSpec.describe Calculator do
         expect(calculator.add(number.to_s)).to  eq(number)
       end
 
+      it 'should give same number as input if only one number in input, with whitespaces in prefix, not in suffix' do
+        calculator = Calculator.new
+        number = rand(100)
+        prefix_spaces = SpecUtils::WhiteSpaceStringGenerator.generate(rand(100))
+        expect(calculator.add("#{prefix_spaces}#{number}")).to  eq(number)
+      end
 
     end
   end
