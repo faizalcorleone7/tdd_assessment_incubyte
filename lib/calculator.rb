ef5af574f807baf88c53d00bcc97471203742471
Roshan Basu <roshanbasu7@gmail.com>
2024-12-28 14:54:28 +0530

    Adding parsing for custom delimiter

--- lib/calculator.rb
+++ lib/calculator.rb
@@ -1,10 +1,38 @@
 class Calculator
+
+  attr_reader :delimiter, :numbers
+
+  def initialize
+    @numbers = []
+  end
+
   def add(input_string)
-    numbers = input_string.split(",").map(&:to_i)
+    parse(input_string)
     sum = 0
     numbers.each { |number|
       sum = sum + number
     }
     sum
   end
+
+  private
+
+  def parse(numbers_string)
+    derive_delimiter(numbers_string)
+    derive_numbers(numbers_string)
+  end
+
+  def derive_delimiter(numbers_string)
+    parsed_delimiter = nil
+    if numbers_string.include?("//")
+      segments = numbers_string.split("//")
+      parsed_delimiter = segments[1].split("\n").first
+    end
+    @delimiter = parsed_delimiter || ","
+  end
+
+  def derive_numbers(numbers_string)
+    @numbers = numbers_string.split(delimiter).map(&:to_i)
+  end
+
 end
